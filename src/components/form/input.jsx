f715f38c5d376f7b8d331382fca7108c7ea46fc5
Souad <220113@fppiramide.com>
2024-03-12 14:17:28 +0100

    up

---
+++ src/components/form/input.jsx
@@ -0,0 +1,13 @@
+import { useFormContext } from 'react-hook-form'
+
+export const Input =({})=> {
+    return(
+        <div>input</div>
+    )
+}
+
+export const errorInput = () =>{
+    return(
+        <div>error</div>
+    )
+}
